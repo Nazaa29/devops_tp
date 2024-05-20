05a2623c7b5936a5a52de60d0dbfd08a739c2988
nazitaOG <lopeznazarenoo@gmail.com>
2024-05-20 19:30:15 -0300

    cucu te amo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hola mundo\n');
+    res.end('Cucu te amo\n');
 });
 
 const port = 3000;
