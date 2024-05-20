3a1476079e81bccdd5d40b8c93b97ee314b0835f
nazitaOG <lopeznazarenoo@gmail.com>
2024-05-20 19:44:12 -0300

    juan 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Tirate una linea pibe dale\n');
+    res.end('Me decis que juego porfiiiiissss?\n');
 });
 
 const port = 3000;
