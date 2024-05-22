3026e16e6c7f5bc72426efba9bbe9cd054bac1c6
nazitaOG <lopeznazarenoo@gmail.com>
2024-05-22 02:30:52 -0300

    pokemon api

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const axios = require('axios');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3001;
+
+// Servir archivos estáticos desde el directorio 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta "Hola Mundo"
 app.get('/', (req, res) => {
-    res.send('Hola Mundo');
+    res.send('Hola Munda');
 });
 
 // Nueva ruta para sumar dos números
@@ -18,6 +23,20 @@ app.get('/sum', (req, res) => {
 
     const sum = num1 + num2;
     res.send({ sum });
+});
+
+// Ruta para obtener una lista de Pokémon
+app.get('/pokemons', async (req, res) => {
+    try {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=10');
+        const pokemons = response.data.results.map((pokemon, index) => ({
+            name: pokemon.name,
+            number: index + 1
+        }));
+        res.send(pokemons);
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to fetch Pokémon data' });
+    }
 });
 
 const server = app.listen(port, () => {
